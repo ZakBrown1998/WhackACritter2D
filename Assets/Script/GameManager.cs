b3e1e01f715673379f867753ea48a3b558659610
ZakBrown1998 <43067354+ZakBrown1998@users.noreply.github.com>
2018-09-21 15:50:19 +0100

    Timer Added

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public List<GameObject> critterPrefabs;
+    public float critterSpawnFrequency = 1.0f;
+public Score scoreDisplay; 
+
+    private float lastCritterSpawn = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float nextSpawnTime = lastCritterSpawn + critterSpawnFrequency;
+        if (Time.time >= lastCritterSpawn + critterSpawnFrequency)
+        {
+            //It is time
+
+            //Choose a random critter to spawn
+            int critterIndex = Random.Range(0, critterPrefabs.Count);
+            GameObject prefabToSpawn = critterPrefabs[critterIndex];
+
+            //Spwan the Critter
+            GameObject spawnCritter = Instantiate(prefabToSpawn);
+
+            //Get access to the critter script
+
+            Critter critterScript = spawnCritter.GetComponent<Critter>();
+
+            //Tell the critter script the score object
+
+            critterScript.scoreDisplay = scoreDisplay;
+
+            //Update the most recent critter spawn time to now
+
+            lastCritterSpawn = Time.time;
+        }
+	}
+}
---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    //Display Visual Timer
+    public TextMesh displayText;
+
+    //Starting time for the timer
+    public float timerDuration;
+
+    //Current Seconds Remaining on the timer
+    private float timeRemaining = 0;
+
+	// Use this for initialization
+	void Start () {
+        StartTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//Only access if timer is running
+    if (IsTimerRunning())
+        {
+            //Time is running, so process
+
+            timeRemaining = timeRemaining - Time.deltaTime;
+
+            //Check if we have now run out of time
+            if(timeRemaining <=0)
+            {
+                StopTimer();
+            }
+            
+            //Update the visual display
+            displayText.text = Mathf.CeilToInt(timeRemaining).ToString();
+        }
+		
+	}
+    //Starts the timer counting
+    public void StartTimer()
+    {
+        timeRemaining = timerDuration;
+        displayText.text = Mathf.CeilToInt(timeRemaining).ToString();
+    }
+
+    //Stop the timer from counting down
+    public void StopTimer()
+    {
+        timeRemaining = 0;
+        displayText.text = Mathf.CeilToInt(timeRemaining).ToString();
+    }
+    //Is the timer currently running?
+
+    public bool IsTimerRunning()
+    {
+        if (timeRemaining !=0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    }
+
