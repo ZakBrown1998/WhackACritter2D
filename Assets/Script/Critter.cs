7baaf7cd3d5c9d20a6ef3a4e8a385022e7a05eb3
ZakBrown1998 <43067354+ZakBrown1998@users.noreply.github.com>
2018-09-21 14:26:55 +0100

    Critter Varitions Added

---
+++ Assets/Script/Critter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Critter : MonoBehaviour {
+
+    public Vector2 lowerRange;
+    public Vector2 upperRange;
+
+    public Score scoreDisplay;
+
+    public int pointValue = 1; //How much is this critter worth?
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(Random.Range(lowerRange.x, upperRange.x),
+            Random.Range(lowerRange.y, upperRange.y),
+            0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //Unity calls this when the game object is clicked on.
+    private void OnMouseDown()
+    {
+        scoreDisplay.ChangeValue(pointValue);
+        Destroy(gameObject);  
+    }
+}
---
+++ Assets/Script/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    //public variables visible in Unity
+    public TextMesh displayText;
+
+    //Private variables cna't be touched by other scripts
+    private int currentValue = 0;
+
+
+
+    // Update both the data value AND the visual display
+    public void ChangeValue(int _toChange)
+    {
+        currentValue = currentValue + _toChange;
+        displayText.text = currentValue.ToString();
+
+
+}
+}
